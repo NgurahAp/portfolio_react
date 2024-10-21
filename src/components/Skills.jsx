282842fdd4c2f3fdb2a8454410aa98d8ceab748e
Arya <ngurahpratama2002@gmail.com>
2024-10-21 10:31:46 +0700

    adding icon on skills page

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,14 +2,24 @@ export const Skills = () => {
   return (
     <section className="h-screen flex flex-col justify-center px-20 text-[#dfdad5] py-28">
       <div className="w-full items-center justify-center">
-        <h1 className="text-center">Core Competencies</h1>
-        <h2 className="text-center">
+        <h1 className="text-center text-4xl font-semibold">
+          Core Competencies
+        </h1>
+        <h2 className="text-center text-lg pt-3">
           Delving into the abilities I harness to create effective solutions.
         </h2>
-        <div>
-          <div>
-            <img src="/expertise/mobile.png" className="h-16" alt="" />
-            <h1>Mobile Dev</h1>
+        <div className="flex gap-10 pt-16 justify-center">
+          <div className=" flex flex-col items-center justify-center">
+            <img src="/expertise/mobile.png" className="h-16 w-auto" alt="" />
+            <h1 className="text-center pt-2 text-lg">Mobile Development</h1>
+          </div>
+          <div className=" flex flex-col items-center">
+            <img src="/expertise/api.png" className="h-16 w-auto" alt="" />
+            <h1 className="text-center pt-2 text-lg">REST API</h1>
+          </div>
+          <div className=" flex flex-col items-center">
+            <img src="/expertise/web.png" className="h-16 w-auto" alt="" />
+            <h1 className="text-center pt-2 text-lg">Web Development</h1>
           </div>
         </div>
       </div>
